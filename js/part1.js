8fa8c5a24ab81efb6fed2a36d46dd50b97257c8b
Byron Huang <bhuang1@hwemail.com>
2020-02-04 10:24:24 -0800

    chng

--- js/part1.js
+++ js/part1.js
@@ -3,12 +3,19 @@ function ConversionPart1() {
     var UnsignedInt = parseInt(document.getElementById("1_UnsignedInt").value);
     var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
     var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
+    var outputValue = "";
+    var input = UnsignedInt;
+    while (UnsignedInt > 0)
+    {
+      outputValue += UnsignedInt % 2;
+      UnsignedInt = Math.floor(UnsignedInt/2);
+    }
 
-
-
-  var outputValue = "11111111";
+    var splitString = outputValue.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
 
   // Show the output on the screen
-  FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
+  FormatAndShowOutput([input, UnsignedIntBaseFrom, UnsignedIntBaseTo, joinArray], 1);
 
 }
--- js/part2.js
+++ js/part2.js
@@ -1,12 +1,49 @@
 function ConversionPart2() {
     //
     var SignedDecimalInt = parseInt(document.getElementById("2_SignedInt").value);
+    var outputValue = "";
+    var outputValueTwosComplement = "";
+    var input = SignedDecimalInt;
+    while (SignedDecimalInt > 0)
+    {
+      outputValue += SignedDecimalInt % 2;
+      outputValueTwosComplement += SignedDecimalInt % 2;
+      SignedDecimalInt = Math.floor(SignedDecimalInt/2);
+    }
+
+    var joinArray3 = outputValueTwosComplement;
+    var outputValueTwosComplement2 = "";
+
+    for (int i = 0 ; i < joinArray3.length; i++)
+    {
+      if ((counter.subString(i,i+1)).equals("1") && (counter.subString(i+1,i+2)).equals("0"))
+      {
+        
+      }
+    }
+
+
+    while (outputValueTwosComplement.length != 23)
+    {
+          outputValueTwosComplement += "1";
+    }
+    while (outputValue.length != 23)
+    {
+        outputValue += "0";
+    }
+
+    var splitString = outputValue.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+
+
+    //var splitString2 = outputValueTwosComplement.split("");
+    //var reverseArray2 = splitString2.reverse();
+    //var joinArray2 = reverseArray2.join("");
 
 
 
-    var outputValue = "010110101000110110011101";
-    var outputValueTwosComplement = "101001010111001001100011";
 
     // Show the output on the screen
-    FormatAndShowOutput([outputValue, outputValueTwosComplement, SignedDecimalInt], 2);
+    FormatAndShowOutput([joinArray, joinArray3, input], 2);
 }
